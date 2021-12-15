d17475e9d82e47ce5340b0e1bb9154be0a67dd7f
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2021-12-14 21:21:27 -0600

    add isPost

--- reddit.php
+++ reddit.php
@@ -17,8 +17,11 @@ $resurl = $decoded->data->children[$randh]->data->url;
 $title = $decoded->data->children[$randh]->data->title;
 $author = $decoded->data->children[$randh]->data->author;
 $self = $decoded->data->children[$randh]->data->selftext;
+
+$isPost = isset($title);
+
 $type = substr($resurl, -3);
-if($type == "jpg" || $type == "png" || $type == "gif" ||  $type == "ico" || $type == "bmp" || $type == "peg" || $limited == 0){
+if($type == "jpg" || $type == "png" || $type == "gif" ||  $type == "ico" || $type == "bmp" || $type == "peg" || $limited == 0 || $isPost){
 echo "<title>" . $title . " (". $author . ")</title> <meta name='description' content='" . $self . "...'><meta name='og:image' content='" . $resurl . "'> <h1>". $title . " (". $author . ")</h1>" . $self ."<br><img style='max-width: 50%; max-height: 50%;' src='" . $resurl . "'>";
 } else {
 echo "An error occurred and our system had selected a post without an image. Please try again!";
